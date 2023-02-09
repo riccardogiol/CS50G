3b2fd69309953fadc6c059227850c6fa21e8bb38
Riccardo Giol <riccardogiol@gmail.com>
2023-02-09 17:44:27 +0100

    B06 preatty colors

--- breakout/src/LevelMaker.lua
+++ breakout/src/LevelMaker.lua
@@ -3,15 +3,43 @@ LevelMaker = Class{}
 function LevelMaker.createMap()
 	local bricks = {}
 	local brick_quads = GenerateBrickQuads(gTextures['main'])
-	local color = math.random(0, 20)
-	local w, h = brick_quads[color]:getQuadDimensions()
-	local numRows = math.random(2, 4)
-	local numCols = math.random(7, 13)
+	local w, h = brick_quads[0]:getQuadDimensions()
+	local numRows = math.random(4, 7)
+	local numCols = math.random(3, 6)*2 + 1
 	for j = 0, numRows - 1 do
 		brick_row = {}
+		local color1 = math.random(0, 4) * 4
+		local color2 = math.random(0, 4) * 4
+		local color = color1
+		local doubleColor = math.random(0, 100) < 50
+		local noBrick = math.random(0, 100)
+		local noPairBrick = noBrick < 20
+		local noUnpairBrick = noBrick > 80
 		for i = 0, numCols - 1 do
 			local x = (VIRTUAL_WIDTH/2 - ((w/2) * numCols)) + i*w
-			brick_row[i] = Brick(brick_quads[color].quad, w, h, x, h*j + 16)
+			if i%2 == 0 then
+				if noPairBrick == false then
+					if doubleColor then
+						if color == color1 then
+							color = color2
+						else
+							color = color1
+						end
+					end
+					brick_row[i] = Brick(brick_quads[color].quad, w, h, x, h*j + 16)
+				end
+			else
+				if noUnpairBrick == false then
+					if doubleColor then
+						if color == color1 then
+							color = color2
+						else
+							color = color1
+						end
+					end
+					brick_row[i] = Brick(brick_quads[color].quad, w, h, x, h*j + 16)
+				end
+			end
 		end
 		bricks[j] = brick_row
 	end
