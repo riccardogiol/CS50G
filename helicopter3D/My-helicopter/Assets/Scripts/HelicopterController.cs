352a4654829a404749890d417a200da5b793b460
Riccardo Giol <riccardogiol@gmail.com>
2023-04-28 13:03:29 +0200

    finish to setup coin system with particles

--- helicopter3D/My-helicopter/Assets/Scripts/HelicopterController.cs
+++ helicopter3D/My-helicopter/Assets/Scripts/HelicopterController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class HelicopterController : MonoBehaviour
@@ -17,6 +18,24 @@ public class HelicopterController : MonoBehaviour
     {
         vertical = Input.GetAxis("Vertical") * speed;
         horizontal = Input.GetAxis("Horizontal") * speed;
+
+        if (transform.position.x < -10)
+        {
+            horizontal = Math.Max(0, horizontal);
+        }
+        if (transform.position.x > 12)
+        {
+            horizontal = Math.Min(0, horizontal);
+        }
+        if (transform.position.y < -3) 
+        {
+            vertical = Math.Max(0, vertical);
+        }
+        if (transform.position.y > 5)
+        {
+            vertical = Math.Min(0, vertical);
+        }
+
         rb.velocity = new Vector3(horizontal, vertical, 0);
     }
 }
