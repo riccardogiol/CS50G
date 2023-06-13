acb7663708277165e21fd19d4ec882841b2d27e7
Riccardo Giol <riccardogiol@gmail.com>
2023-06-13 16:30:05 +0200

    portal assignement

---
+++ portal/Assets/Scripts/GameOverTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverTrigger : MonoBehaviour {
+	public bool gameOver = false;
+
+	void Start () {}
+	void Update () {}
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.gameObject.tag == "Player") {
+			gameOver = true;
+		}
+	}
+}
